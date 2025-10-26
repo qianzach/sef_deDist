94cb926197f67a56d2dd1f45cc4fd8452a87653b
Zach Qian <43225577+qianzach@users.noreply.github.com>
2025-10-26 00:16:54 -0400

    Add files via upload

--- RDA/SLE_validation_comparison_perez_10_21_2025.r
+++ RDA/SLE_validation_comparison_perez_10_21_2025.r
@@ -7,9 +7,9 @@ source("/Users/zaqian/Desktop/density_estimation/sef_lupus_results/exploration/t
 
 perez = read.csv("./density_estimation/sef_lupus_results/exploration/perez_validation_DEGs.csv", row.names = 1)
 covariate_df = read.csv("./density_estimation/sef_lupus_results/exploration/to_github_repo/DATA/covariate_data.csv")
-donors_cd8 = readRDS("/Users/zaqian/Desktop/density_estimation/sef_lupus_results/exploration/to_github_repo/objects/cd8_donors_for_genes_processed.RDS") #renamed to cd8 for consistency, original was "updated_cd8"
-donors_cd4 = readRDS("/Users/zaqian/Desktop/density_estimation/sef_lupus_results/exploration/to_github_repo/objects/cd4_donors_for_genes_processed.RDS") #renamed to cd4 for consistency, original was "TEST_cd4"
-donors_cM = readRDS("./density_estimation/sef_lupus_results/exploration/to_github_repo/objects/cM_donors_for_genes_processed.RDS") #renamed to cM for consistency, original was "monocytes"
+donors_cd8 = readRDS("/Users/zaqian/Desktop/density_estimation/sef_lupus_results/exploration/to_github_repo/objects/cd8_donors_for_genes_processed.RDS") 
+donors_cd4 = readRDS("/Users/zaqian/Desktop/density_estimation/sef_lupus_results/exploration/to_github_repo/objects/cd4_donors_for_genes_processed.RDS") 
+donors_cM = readRDS("./density_estimation/sef_lupus_results/exploration/to_github_repo/objects/cM_donors_for_genes_processed.RDS") 
 cd8_validated_genes = intersect(rownames(perez)[perez$T8 == 1], names(donors_cd8))
 cd4_validated_genes= intersect(rownames(perez)[perez$T4 == 1], names(donors_cd4))
 cM_validated_genes = intersect(rownames(perez)[perez$cM == 1], names(donors_cM))
